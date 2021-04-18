f381a25f96b035f6dcc49ffef4a23eadcab56041
gavinrufus <61418686+gavinrufus@users.noreply.github.com>
2021-04-18 14:53:33 +0300

    commit

--- stages/2/client/index.js
+++ stages/2/client/index.js
@@ -1,5 +1,31 @@
+function removeContentFrom(what) {
+  what.textContent = '';
+}
+
+function showMessages(Items, where) {
+  for (const Item of Items) {
+    const li = document.createElement('li');
+    li.textContent = Item;
+    where.append(li);
+  }
+}
+
+async function loadItems() {
+  const response = await fetch('Items');
+  let Items;
+  if (response.ok) {
+    Items = await response.json();
+  } else {
+    Items = ['failed to load messages :-('];
+  }
+
+  const itemlist = document.querySelector('#itemslist');
+  removeContentFrom(itemlist);
+  showIteams(items, itemlist);
+}
+
 function pageLoaded() {
-  console.log('The file has loaded');
+  loadItems();
 }
 
 window.addEventListener('load', pageLoaded);
